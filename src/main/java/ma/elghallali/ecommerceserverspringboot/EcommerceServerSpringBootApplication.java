d2409a41ee3c531c2b01df7127525173e23c3b29
Yassine EL GHALLALI <35077623+elghallali@users.noreply.github.com>
2022-06-23 19:05:42 +0100

    create default project

---
+++ src/main/java/ma/elghallali/ecommerceserverspringboot/EcommerceServerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ma.elghallali.ecommerceserverspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/elghallali/ecommerceserverspringboot/EcommerceServerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elghallali.ecommerceserverspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
